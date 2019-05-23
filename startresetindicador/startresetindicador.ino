2863a348a063d5d9b4491383f6beb44b84ad4854
Officiallumpia <50963430+Officiallumpia@users.noreply.github.com>
2019-05-23 11:26:07 -0700

    Add files via upload

---
+++ startresetindicador/startresetindicador.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+
+Servo servoLeft;
+Servo servoRight;
+
+
+
+void setup() {
+tone(4,300,1000);
+delay(1000);
+
+Serial.begin(9600);
+servoLeft.attach(13);
+
+}
+
+void loop()
+{
+  // loop counts with pulseWidth from 1375 to 1625 in increments of 25.
+  for (int pulseWidth = 1375; pulseWidth <= 1625; pulseWidth += 25)
+  {
+  Serial.print("pulseWidth = ");
+  Serial.println(pulseWidth);
+  
+  Serial.println("Press a key and click");
+  Serial.println("Send to start servo...");
+
+  while(Serial.available() == 0);
+  Serial.read();
+
+  Serial.println("Running...");
+  servoLeft.writeMicroseconds(1500);
+}
+}
+
